eab817a2ff7673b8e074f629998b6209b2b2676e
limyongtae <lyt032410@gmail.com>
2019-12-20 16:15:54 +0900

    exception 처리 경로 문제 해결

--- init/softhsm/softhsm-install.sh
+++ init/softhsm/softhsm-install.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
   
-@echo "install softhsm2..."
+echo "install softhsm2..."
 wget https://dist.opendnssec.org/source/softhsm-2.5.0.tar.gz
 tar -xzf softhsm-2.5.0.tar.gz
 ./softhsm-2.5.0/configure --disable-gost 
 make -f ./Makefile
 sudo make install -f ./Makefile
-cp /usr/local/lib/softhsm/libsofthsm2.so ../..//softhsm/lib/.
+mkdir -p ../../softhsm/lib
+cp /usr/local/lib/softhsm/libsofthsm2.so ../../softhsm/lib/.
